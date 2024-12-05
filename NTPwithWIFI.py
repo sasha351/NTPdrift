683c5a7e240b2328f361b79e42d865de518de63d
thocmurphy <thocmurphy@umass.edu>
2024-12-05 15:24:56 -0500

    NTPwithWIFI.py sets the boards time to NTP time

---
+++ NTPwithWIFI.py
@@ -0,0 +1,59 @@
+import network
+import socket
+import time
+import struct
+import yaml
+
+# from machine import Pin
+import machine
+
+with open("credentials.yaml", "r") as file:
+    credentials = yaml.safe_load(file)
+
+NTP_DELTA = 2208988800
+host = "pool.ntp.org"
+
+led = machine.Pin("LED", machine.Pin.OUT)
+
+ssid = credentials["Wifi_Name"]
+password = credentials["Wifi_Password"]
+
+def set_time():
+    NTP_QUERY = bytearray(48)
+    NTP_QUERY[0] = 0x1B
+    addr = socket.getaddrinfo(host, 123)[0][-1]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.settimeout(1)
+        res = s.sendto(NTP_QUERY, addr)
+        msg = s.recv(48)
+    finally:
+        s.close()
+    val = struct.unpack("!I", msg[40:44])[0]
+    t = val - NTP_DELTA    
+    tm = time.gmtime(t)
+    machine.RTC().datetime((tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5], 0))
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+
+max_wait = 10
+while max_wait > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    max_wait -= 1
+    print('waiting for connection...')
+    time.sleep(1)
+
+if wlan.status() != 3:
+    raise RuntimeError('network connection failed')
+else:
+    print('connected')
+    status = wlan.ifconfig()
+    print( 'ip = ' + status[0] )
+
+led.on()
+set_time()
+print(time.localtime())
+led.off()
